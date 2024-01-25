2a908b40b94ec8e8c8b112266053cdf7c0ceb5fa
Patrick King <pat.k195@gmail.com>
2024-01-25 16:54:18 -0500

    commit

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # Scientific-python-libraries
 Python ecosystem for scientific computing (main libraries)
+
+Working on Pandas Right now
---
+++ pandas_example.py
@@ -0,0 +1,30 @@
+import pandas as pd
+
+#sample pandas project
+
+#show max columns
+#pd.set_option('display.max_columns', None)
+
+results = pd.ExcelFile("Lab.xlsx")
+constantRun = pd.read_excel(results, 'Table1')
+intervalSprint = pd.read_excel(results, 'Table3')
+
+
+###################################
+#read the first row
+#print (results.head(1))
+###################################
+
+###################################
+#return info on the data table
+#print (results.info())
+###################################
+
+###################################
+#print a row from a specific table
+#part1time = constantRun["Time"]
+#part2Time = intervalSprint["Time"]
+###################################
+
+###################################
+#
